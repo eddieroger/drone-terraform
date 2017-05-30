ca06ee15542ee6146063745031a37240e6b1c88f
Jacob McCann <jacob.mccann2@target.com>
2017-05-30 09:48:34 -0500

    Remove secrets handling and add copying TF_VAR_ vars to lowercase names

---
+++ plugin_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os"
+	"testing"
+	. "github.com/franela/goblin"
+)
+
+func TestPlugin(t *testing.T) {
+	g := Goblin(t)
+
+	g.Describe("CopyTfEnv", func() {
+		g.It("Should create copies of TF_VAR_ to lowercase", func() {
+			// Set some initial TF_VAR_ that are uppercase
+			os.Setenv("TF_VAR_SOMETHING", "some value")
+			os.Setenv("TF_VAR_SOMETHING_ELSE", "some other value")
+
+			CopyTfEnv()
+
+			// Make sure new env vars exist with proper values
+			g.Assert(os.Getenv("TF_VAR_something")).Equal("some value")
+			g.Assert(os.Getenv("TF_VAR_something_else")).Equal("some other value")
+		})
+	})
+}
